0d5731e040180d476b5d298ea01a8e1dd58c83fa
jachavesr <54504439+jachavesr@users.noreply.github.com>
2026-05-20 21:22:25 -0500

    correos

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,7 @@ LOGIN_REDIRECT_URL = '/gestion/horario/'
 LOGOUT_REDIRECT_URL = '/gestion/horario/'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp-relay.sendinblue.com'
 EMAIL_PORT = 587
